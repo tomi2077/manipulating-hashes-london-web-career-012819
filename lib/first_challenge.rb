b801bc81c5b1da782b2c9f0c2daf4d4c93dd7cfa
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2018-12-27 01:28:42 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -16,7 +16,7 @@ def first_challenge
   contacts.each do |person, data|
     data.each do |attribute, value|
       if attribute == :favorite_icecream_flavors
-        
+        if :favorite_icecream_flavors
     
 
 
